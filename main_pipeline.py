47ace0cea5cee8f881cdb8f90d40fcbab95bf57d
shardsnaik <sharadsanik001@gmail.com>
2025-06-05 17:52:47 +0530

    data building

--- main_pipeline.py
+++ main_pipeline.py
@@ -13,31 +13,31 @@ class  the_AI_Kiosks:
         self.history_obj =  ChatManagerPipeline()
 
     def run_main(self):
-        # # Initiating menu manager pipeline
-        # obj = MenuManagerPipeline()
-        # obj.run_pipeline()
+        # Initiating menu manager pipeline
+        obj = MenuManagerPipeline()
+        obj.run_pipeline()
         
         # Initiating speech recognition pipeline
         # speech_recog_obj = speech_recognition_pipeline()
         # speech_text = speech_recog_obj.main()
 
         # Initiating LLM manager pipeline ( which is internally connected with speech recognition pipeline) 
-        llm_obj = llm_maneger_pipeline()
-        while True:
-            ques, res = llm_obj.main()
+        # llm_obj = llm_maneger_pipeline()
+        # while True:
+        #     ques, res = llm_obj.main()
 
-            # Initiating intent recognition pipeline 
-            '''
-            res is the text generated from llm  and the user voice input both are used to extract the intent
-            '''
-            self.intent_recog_obj.intent_recog_main(ques)
-            self.tts_obj.main(res)
+        #     # Initiating intent recognition pipeline 
+        #     '''
+        #     res is the text generated from llm  and the user voice input both are used to extract the intent
+        #     '''
+        #     self.intent_recog_obj.intent_recog_main(ques)
+        #     self.tts_obj.main(res)
 
-            # pipeline for saving the chat history
-            self.history_obj.chat_history_main(user_text=ques, bot_text=res)
+        #     # pipeline for saving the chat history
+        #     self.history_obj.chat_history_main(user_text=ques, bot_text=res)
 
-            if ques in ['exit', 'quit']:
-                break
+        #     if ques in ['exit', 'quit']:
+        #         break
 
 
 
